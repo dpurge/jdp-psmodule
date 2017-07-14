60f906bae9f36b38319b4d78a6483748478dbc39
David P <david.purge@gmail.com>
2017-07-14 02:51:45 +0200

    Add class for creating WPF applications

---
+++ src/JdpSystem/Invoke-Application.ps1
@@ -0,0 +1,20 @@
+Add-Type -AssemblyName PresentationFramework, System.Drawing, System.Windows.Forms, WindowsFormsIntegration
+
+
+<#
+$base64 = ""
+
+
+
+$bitmap = New-Object System.Windows.Media.Imaging.BitmapImage
+$bitmap.BeginInit()
+$bitmap.StreamSource = [System.IO.MemoryStream][System.Convert]::FromBase64String($base64)
+$bitmap.EndInit()
+$bitmap.Freeze()
+#>
+
+
+. $PSScriptRoot\class\WpfApplication.ps1
+$Application = [WpfApplication]::new($pid, "$PSScriptRoot\xaml\MainWindow.xaml")
+#$Application.setIcon($bitmap)
+$Application.Run()
---
+++ src/JdpSystem/class/WpfApplication.ps1
@@ -0,0 +1,44 @@
+class WpfApplication
+{
+	$process
+    [xml] $xaml
+	$window
+	$widget
+	$context
+	
+	WpfApplication($processId, [String] $xamlFile)
+	{
+	    $this.process = Get-Process -PID $processId
+	    $this.xaml = [xml](Get-Content $xamlFile)
+		$this.window = [Windows.Markup.XamlReader]::Load((New-Object System.Xml.XmlNodeReader $this.xaml))
+		$this.widget = @{}
+		foreach ($i in $this.xaml.SelectNodes("//*[@Name]")) {
+		    $this.widget.add(($i.Name), $this.window.FindName($i.Name))
+		}
+		$this.window.Add_Closed({
+			[System.Windows.Forms.Application]::Exit()
+		})
+		
+		$this.context = New-Object System.Windows.Forms.ApplicationContext 
+	}
+	
+	setIcon($bitmap) {
+	    $this.window.Icon = $bitmap
+		$this.window.TaskbarItemInfo.Overlay = $bitmap
+        $this.window.TaskbarItemInfo.Description = $this.window.Title
+	}
+	
+	Run()
+	{
+	    $asyncwindow = Add-Type -name Win32ShowWindowAsync -namespace Win32Functions -PassThru -MemberDefinition '[DllImport("user32.dll")] public static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);'
+		try {
+		    $asyncwindow::ShowWindowAsync($this.process.MainWindowHandle, 0) | Out-Null
+			[System.Windows.Forms.Integration.ElementHost]::EnableModelessKeyboardInterop($this.window)
+			$this.window.Show()
+			$this.window.Activate()
+			[void][System.Windows.Forms.Application]::Run($this.context)
+		} finally {
+		    $asyncwindow::ShowWindowAsync($this.process.MainWindowHandle, 3) | Out-Null
+		}
+	}
+}
