5f3a17685f2f8aa387ff0260e73a78d8a1e6eb14
Janusz Prusaczyk (JJPR) <jjpr@demant.com>
2020-11-09 16:40:18 +0100

    Add Book2Print

---
+++ src/JdpBookbind/Configuration.psd1
@@ -0,0 +1,17 @@
+@{
+    Tools = @{
+        ImageMagick = "D:\pgm\ImageMagick\magick.exe"
+        CJB2 = "D:\pgm\DjVuLibre\cjb2.exe"
+        CpalDjvu = "D:\pgm\DjVuLibre\cpaldjvu.exe"
+        Djvm = "D:\pgm\DjVuLibre\djvm.exe"
+        DDjVu = "D:\pgm\DjVuLibre\ddjvu.exe"
+        PdfImages = "D:\pgm\poppler\bin\pdfimages.exe"
+        PDFtk = "E:\pgm\PDFtk\bin\pdftk.exe"
+    }
+
+    Blank = @{
+        Pdf = @{
+            a5 = ".\data\blank-page\pdf\a5.pdf"
+        }
+    }
+}
--- src/JdpBookbind/Invoke-Book2Print.ps1
+++ src/JdpBookbind/Invoke-Book2Print.ps1
@@ -7,6 +7,8 @@ Function Invoke-Book2Print {
 		[int]    $Sheets = 8
 	)
 
+	$config = Import-PowerShellDataFile $PSScriptRoot\Configuration.psd1
+
 	$PagesPerGathering = 4 * $Sheets
     
     [System.IO.Directory]::SetCurrentDirectory(((Get-Location -PSProvider FileSystem).ProviderPath))
@@ -21,7 +23,9 @@ Function Invoke-Book2Print {
 	}
 
 	$Index = 0
+	$Counter = 0
 	while ($Index -lt $Pages.Length - 1) {
+		$Counter++
 		$Gathering = $Pages[$Index .. ($Index + $PagesPerGathering - 1)]
 
 		$PrintSequence = @()
@@ -33,10 +37,31 @@ Function Invoke-Book2Print {
 			$end -= 2
 		}
 
+		$infile = $BookFile.FullName
+		$outfile = Join-Path `
+			-Path $OutputDirectory `
+			-ChildPath ("{0}-{1:D3}.pdf" -f $BookFile.BaseName, $Counter)
+
 		switch ($BookFile.Extension) {
 			'.pdf' {
+				$BlankPage = Join-Path -Path $PsScriptRoot -ChildPath $config.Blank.Pdf.a5
 				if ( -not $OutputDirectory.Exists) { $OutputDirectory.Create() }
-				Write-Host ($PrintSequence -join '-')
+				$pgm = $config.Tools.PDFtk
+				$pgmParam =  ,"A=`"${infile}`""
+				$pgmParam +=  ,"B=`"${BlankPage}`""
+				$pgmParam +=  ,"cat"
+				$pgmParam +=  ($PrintSequence | ForEach-Object { if($_) {"A${_}"} else {"B1"} })
+				$pgmParam +=  ,"output"
+				$pgmParam +=  ,"`"${outfile}`""
+				& $pgm @pgmParam
+				if ($?) {
+					Write-Output $outfile
+				} else {
+					$msg = $Error[0].Exception.Message
+					Write-Error "Error in chunk ${Counter}: ${msg}"
+					#exit
+				}
+				
 			}
 			default {
 				throw "Unsupported file format: $($BookFile.Extension)"
